3256bd9014bb943e2df7c381a998e5c1992146f0
Laboratorio <laboratorio@udla-ec.int>
2026-05-20 11:40:48 -0500

    Eduardo Cisneros v5

--- src/main/java/ec/edu/poo/Servidor.java
+++ src/main/java/ec/edu/poo/Servidor.java
@@ -7,15 +7,14 @@ public class Servidor extends ActivoDigital {
     public Servidor(String codigo, String nombre, int nivelRiesgo,
                     boolean parcheAplicado, String sistemaOperativo) {
         super(codigo, nombre, nivelRiesgo, parcheAplicado);
-        // TODO: completar constructor
+        this.sistemaOperativo = sistemaOperativo;
     }
 
     public String getSistemaOperativo() {
-        // TODO: completar
-        return null;
+        return this.sistemaOperativo;
     }
 
     public void setSistemaOperativo(String sistemaOperativo) {
-        // TODO: completar
+        this.sistemaOperativo = sistemaOperativo;
     }
 }
