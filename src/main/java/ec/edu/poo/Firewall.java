2b58e31906759877e8325dde25ade046a4752ee2
Laboratorio <laboratorio@udla-ec.int>
2026-05-20 11:32:17 -0500

    Eduardo Cisneros 3

--- src/main/java/ec/edu/poo/Firewall.java
+++ src/main/java/ec/edu/poo/Firewall.java
@@ -7,15 +7,14 @@ public class Firewall extends ActivoDigital {
     public Firewall(String codigo, String nombre, int nivelRiesgo,
                     boolean parcheAplicado, int reglasActivas) {
         super(codigo, nombre, nivelRiesgo, parcheAplicado);
-        // TODO: completar constructor
+        this.reglasActivas = reglasActivas;
     }
 
     public int getReglasActivas() {
-        // TODO: completar
-        return 0;
+        return this.reglasActivas;
     }
 
     public void setReglasActivas(int reglasActivas) {
-        // TODO: completar
+        this.reglasActivas = reglasActivas;
     }
 }
